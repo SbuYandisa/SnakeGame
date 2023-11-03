9635fcb6a49fe8396d9c4bf38f245041c0b48b11
SbuYandisa <madingayandisa@gmail.com>
2023-11-03 17:03:58 +0200

    Updated new files

---
+++ Snake/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB7FvPSVBN02l_9befcuY6ruVCyGqjdu6I",
+    authDomain: "snake-a96c8.firebaseapp.com",
+    projectId: "snake-a96c8",
+    storageBucket: "snake-a96c8.appspot.com",
+    messagingSenderId: "689483691928",
+    appId: "1:689483691928:web:490e97eeca5b3b98275ded",
+    measurementId: "G-4QLM4KS7E0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
